4d843f98c14b3beb980dfe7456c3e1eaf858c161
qhuydtvt <qhuydtvt@gmail.com>
2017-01-10 01:44:31 +0700

    Added Task Progress

--- app.py
+++ app.py
@@ -20,8 +20,11 @@ parser.add_argument("color", type=str, help="Color of task")
 parser.add_argument("payment_per_hour", type=float, help="Payment rate of task")
 parser.add_argument("done", type=bool, help="Status of task")
 
+parser.add_argument("task_id", type=str, help="Id of task")
+parser.add_argument("date", type=str, help="Date of progress")
+parser.add_argument("duration_in_secs", type=int, help="Duration of task progress")
 
-class TaskListResource(Resource):
+class TaskListRes(Resource):
     def get(self):
         tasks = Task.objects()
         return [task.get_json() for task in tasks], 200
@@ -45,7 +48,7 @@ class TaskListResource(Resource):
         task.save()
         return task.get_json(), 201
 
-class TaskResource(Resource):
+class TaskRes(Resource):
 
     def get(self, task_id):
         task = Task.objects().with_id(task_id)
@@ -84,8 +87,59 @@ class TaskResource(Resource):
                         )
             return Task.objects().with_id(task.id).get_json(), 200
 
-api.add_resource(TaskListResource, "/api/task")
-api.add_resource(TaskResource, "/api/task/<task_id>")
+
+class TaskProgressListRes(Resource):
+    def get(self):
+        return [task_progress.get_json() for task_progress in TaskProgress.objects()], 200
+
+
+    def post(self):
+        args = parser.parse_args()
+        task_id = args["task_id"]
+        date = utils.date_from_iso8601(args["date"])
+        duration_in_secs = args["duration_in_secs"]
+
+        task = Task.objects().with_id(task_id)
+        if task is None:
+            return {"code": 0, "message": "Task not found"}, 404
+        else:
+            task_progress = TaskProgress(task=task, date=date, duration_in_secs=duration_in_secs)
+            task_progress.save()
+            return TaskProgress.objects().with_id(task_progress.id).get_json(), 200
+
+class TaskProgressRes(Resource):
+    def get(self, task_progress_id):
+        task_progress = TaskProgress.objects().with_id(task_progress_id)
+        if task_progress is None:
+            return {"code": 0, "message": "Task progress not found"}, 404
+        else:
+            return task_progress.get_json(), 200
+
+    def put(self, task_progress_id):
+        task_progress = TaskProgress.objects().with_id(task_progress_id)
+        if task_progress is None:
+            return {"code": 0, "message": "Task progress not found"}, 404
+        else:
+            args = parser.parse_args()
+            date = utils.date_from_iso8601(args["date"])
+            duration_in_secs = args["duration_in_secs"]
+            task_progress.update(set__date=date, set__duration_in_secs=duration_in_secs)
+            return TaskProgress.objects().with_id(task_progress.id).get_json(), 200
+
+    def delete(self, task_progress_id):
+        task_progress = TaskProgress.objects().with_id(task_progress_id)
+        if task_progress is None:
+            return {"code": 0, "message": "Task progress not found"}, 404
+        else:
+            task_progress.delete()
+            return {"code": 1, "message": "Task progress deleted"}, 200
+
+api.add_resource(TaskListRes, "/api/task")
+api.add_resource(TaskRes, "/api/task/<task_id>")
+
+api.add_resource(TaskProgressListRes, "/api/task-progress")
+api.add_resource(TaskProgressRes, "/api/task-progress/<task_progress_id>")
+
 
 if __name__ == '__main__':
     app.run(port=6969)
--- task_progress.py
+++ task_progress.py
@@ -1,8 +1,17 @@
 from mongoengine import *
 from task import *
+from datetime import datetime
 
 
 class TaskProgress(Document):
     task = EmbeddedDocumentField("Task")
     date = DateTimeField()
     duration_in_secs = LongField()
+
+    def get_json(self):
+        return {
+            "id": str(self.id),
+            "task": self.task.get_json(),
+            "date": utils.date_from_str(str(self.date)).isoformat(),
+            "duration_in_secs": self.duration_in_secs
+        }
